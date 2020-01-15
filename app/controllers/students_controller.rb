9200db97a956ff22bc776c903f6166035b4bb54c
angelopoole <angelo.poole@outlook.com>
2020-01-15 18:20:26 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,21 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @students = Student.find(params[:id])
+  end
+
+  # def my_movies
+  #   @student = Student.find(params[:id]) 
+  #   @my_movie_list = @student.movies
+
+  #   render :personal_list
+  # end 
+
+  def activate
+    @student = Student.find(params[:id]) #gets the student and asigns it to @students
+    @student.active = !@student.active #
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.destroy_all
+
+10.times do
+    Student.create(first_name:Faker::Games::LeagueOfLegends.unique.champion  , last_name: Faker::Games::LeagueOfLegends.rank)
+end
